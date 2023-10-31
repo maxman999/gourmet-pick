7c087a9573a8c60daf14c25bad9350f829032bab
maxman999 <gom_55@naver.com>
2023-10-31 11:37:48 +0900

    1. customErrorController 추가

--- src/main/java/com/kjy/gourmet/web/CustomErrorController.java
+++ src/main/java/com/kjy/gourmet/web/CustomErrorController.java
@@ -28,7 +28,7 @@ public class CustomErrorController implements ErrorController {
         model.addAttribute("msg", httpStatus.getReasonPhrase());
         model.addAttribute("timestamp", new Date());
 
-        return "/error";
+        return "error";
     }
 
 }
